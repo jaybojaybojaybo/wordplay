001252fb639770ff978122f78bf5f012cd069f5a
Griffin P Longtin and jasun feddema <griffin@>
2017-03-06 20:24:45 -0800

    build the Griffin says or did not say website.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("form").submit(function() {
+    event.preventDefault();
+    var textInput = $("input#textInput").val();
+    var textInput2 = $("input#textInput").val();
+    if (textInput === ("Good Soup" || "Lit!" || "Saw, dude!")) {
+        $("#input").text(textInput);
+        $("griffin").show();
+    } else {
+        $("#input2").text(textInput2);
+        $("griffinNo").show();
+    }
+    $("form").hide();
+  });
+});
